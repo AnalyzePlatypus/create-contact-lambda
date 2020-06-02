a74ee43b8f97ffe03561dd33e6f747935cb90fb5
Michoel Samuels <k2co3@icloud.com>
2020-06-03 01:23:00 +0300

    Redact deploy script

--- create-contact/deploy.sh
+++ create-contact/deploy.sh
@@ -1,9 +1,12 @@
+export $(egrep -v '^#' .env | xargs)
+echo $LAMBDA_FUNCTION_NAME
+cd create-contact
 rm function.zip 
 zip -r function.zip app.js node_modules package.json
 echo "🌀 Uploading..."
 aws lambda update-function-code \
-  --function-name=GrowthShakeCollectLeads \
+  --function-name=$LAMBDA_FUNCTION_NAME \
   --zip-file=fileb://function.zip \
-  --profile=GrowthShakeCollectLeadsLambdaUser && \
-terminal-notifier -title 'GrowthShake Lambda' -message '✅ Deploy Complete' -open 'https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions/asset-search-index-regenerator' -appIcon '/Users/developer/workspace/loanmower-frontend/S3 Logo.png' && \
-curl -X POST -H 'Content-type: application/json' --data '{"text":"🚀 GrowthShake Lambda redeployed"}' https://hooks.slack.com/services/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
+  --profile=$AWS_UPLOAD_CODE_PROFILE && \
+terminal-notifier -title $LAMBDA_FUNCTION_NAME -message '✅ Deploy Complete' -open "https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions/${LAMBDA_FUNCTION_NAME}" -appIcon $NOTIFICATION_ICON_PATH && \
+curl -X POST -H 'Content-type: application/json' --data '{"text":"🚀 Lambda redeployed"}' $SLACK_DEV_NOTIFICATIONS_WEBHOOK_URL
